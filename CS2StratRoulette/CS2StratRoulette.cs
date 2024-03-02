18f0f05245e7d9f5ee2b7e8753ae316bf8eeb140
biggestmannest <54690435+biggestmannest@users.noreply.github.com>
2024-03-03 00:43:54 +0100

    updates

--- CS2StratRoulette/CS2StratRoulette.cs
+++ CS2StratRoulette/CS2StratRoulette.cs
@@ -1,7 +1,16 @@
-﻿namespace CS2StratRoulette
-{
-	public static class CS2StratRoulette
-	{
+﻿using CounterStrikeSharp.API.Core;
 
+namespace CS2StratRoulette
+{
+	public class CS2StratRoulette : BasePlugin
+	{
+		public override string ModuleName => "CS2StratRoulette";
+
+		public override string ModuleVersion => "0.0.1";
+
+		public override void Load(bool hotReload)
+		{
+			System.Console.WriteLine("i hate windows");
+		}
 	}
 }
