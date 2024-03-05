73bb9b65a7a8cfeadb64ea637a4e9fd6858fb460
biggestmannest <54690435+biggestmannest@users.noreply.github.com>
2024-03-05 22:30:28 +0100

    refactor

--- CS2StratRoulette/Strategies/FakeCheats.cs
+++ CS2StratRoulette/Strategies/FakeCheats.cs
@@ -1,86 +1,87 @@
-using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Modules.Timers;
+using CounterStrikeSharp.API;
 using System.Diagnostics.CodeAnalysis;
-using System;
 
 namespace CS2StratRoulette.Strategies
 {
-    [SuppressMessage("ReSharper", "UnusedType.Global")]
-    public class FakeCheats : IStrategy
-    {
-        /// <inheritdoc cref="IStrategy.Name"/>
-        public string Name => "Fake Cheats";
+	[SuppressMessage("ReSharper", "UnusedType.Global")]
+	public class FakeCheats : IStrategy
+	{
+		private const float Interval = 3.0f;
 
-        /// <inheritdoc cref="IStrategy.Description"/>
-        public string Description => "Spams fake cheat advertisements in the chat.";
+		private static readonly string[] Messages =
+		{
+			"NEVER VAC AND YOU KNOW! ONLY @ GOGlobal.cheat",
+			"AIM, WALLS & MORE @ GOGlobal.cheat",
+			"50% OFF ONLY ON GOGlobal.cheat",
+			"FREE 14 DAY SPINBOT TRIAL @ GOGlobal.cheat",
+			"VISIT GOGlobal.cheat TODAY TO DOWNLOAD BEST HACKS",
+			"VAC UNDEDECTED SOFT AIM @ GOGlobal.cheat",
+			"UNDETECTED AIMBOT & WALLHACKS @ GOGlobal.cheat",
+			"EXCLUSIVE ESP & TRIGGERBOT @ GOGlobal.cheat",
+			"LIMITED TIME OFFER: 60% DISCOUNT @ GOGlobal.cheat",
+			"TRY OUR AIM ASSISTANCE TODAY @ GOGlobal.cheat",
+			"INSTANT HEADSHOTS GUARANTEED @ GOGlobal.cheat",
+			"UNDISTURBED BY VAC BANS @ GOGlobal.cheat",
+			"EXPLORE OUR ESP FEATURES @ GOGlobal.cheat",
+			"UNTRACEABLE AIMBOT TECHNOLOGY @ GOGlobal.cheat",
+			"ENHANCE YOUR GAMING EXPERIENCE @ GOGlobal.cheat",
+			"UNDISCOVERED CHEAT METHODS @ GOGlobal.cheat",
+		};
 
-        /// <inheritdoc cref="IStrategy.Running"/>
-        public bool Running { get; private set; }
+		/// <inheritdoc cref="IStrategy.Name"/>
+		public string Name => "Fake Cheats";
 
-        private static string[] fakeMsgs = {
-                "NEVER VAC AND YOU KNOW! ONLY @ GOGlobal.cheat",
-                "AIM, WALLS & MORE @ GOGlobal.cheat",
-                "50% OFF ONLY ON GOGlobal.cheat",
-                "FREE 14 DAY SPINBOT TRIAL @ GOGlobal.cheat",
-                "VISIT GOGlobal.cheat TODAY TO DOWNLOAD BEST HACKS",
-                "VAC UNDEDECTED SOFT AIM @ GOGlobal.cheat",
-                "UNDETECTED AIMBOT & WALLHACKS @ GOGlobal.cheat",
-                "EXCLUSIVE ESP & TRIGGERBOT @ GOGlobal.cheat",
-                "LIMITED TIME OFFER: 60% DISCOUNT @ GOGlobal.cheat",
-                "TRY OUR AIM ASSISTANCE TODAY @ GOGlobal.cheat",
-                "INSTANT HEADSHOTS GUARANTEED @ GOGlobal.cheat",
-                "UNDISTURBED BY VAC BANS @ GOGlobal.cheat",
-                "EXPLORE OUR ESP FEATURES @ GOGlobal.cheat",
-                "UNTRACEABLE AIMBOT TECHNOLOGY @ GOGlobal.cheat",
-                "ENHANCE YOUR GAMING EXPERIENCE @ GOGlobal.cheat",
-                "UNDISCOVERED CHEAT METHODS @ GOGlobal.cheat"
-            };
+		/// <inheritdoc cref="IStrategy.Description"/>
+		public string Description => "Spams fake cheat advertisements in the chat.";
 
-        private Timer? timer;
+		/// <inheritdoc cref="IStrategy.Running"/>
+		public bool Running { get; private set; }
 
-        /// <inheritdoc cref="IStrategy.Start"/>
-        public bool Start(ref CS2StratRoulettePlugin plugin)
-        {
-            if (this.Running)
-            {
-                return false;
-            }
+		private readonly System.Random random = new();
 
-            var random = new Random();
-            var players = Utilities.GetPlayers();
-            this.timer = new Timer(3.0f, () =>
-            {
-                int index = random.Next(fakeMsgs.Length);
-                var player = players[random.Next(0, players.Count)];
-                string randomMsg = fakeMsgs[index];
-                Console.WriteLine("asdasdasddddddasdasd");
-                if (player.IsValid)
-                {
-                    player.ExecuteClientCommandFromServer($"say {randomMsg}");
-                }
-            }, TimerFlags.REPEAT);
+		private Timer? timer;
 
-            this.Running = true;
+		/// <inheritdoc cref="IStrategy.Start"/>
+		public bool Start(ref CS2StratRoulettePlugin plugin)
+		{
+			if (this.Running)
+			{
+				return false;
+			}
 
-            return true;
-        }
+			var players = Utilities.GetPlayers();
 
-        /// <inheritdoc cref="IStrategy.Stop"/>
-        public bool Stop(ref CS2StratRoulettePlugin plugin)
-        {
-            if (!this.Running)
-            {
-                return false;
-            }
+			this.timer = new Timer(FakeCheats.Interval, () =>
+			{
+				var player = players[this.random.Next(0, players.Count)];
 
-            this.Running = false;
+				if (player.IsValid)
+				{
+					var message = FakeCheats.Messages[this.random.Next(FakeCheats.Messages.Length)];
 
-            if (this.timer is not null)
-            {
-                this.timer.Kill();
-            }
+					player.ExecuteClientCommandFromServer($"say {message}");
+				}
+			}, TimerFlags.REPEAT);
 
-            return true;
-        }
-    }
+			this.Running = true;
+
+			return true;
+		}
+
+		/// <inheritdoc cref="IStrategy.Stop"/>
+		public bool Stop(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!this.Running)
+			{
+				return false;
+			}
+
+			this.Running = false;
+
+			this.timer?.Kill();
+
+			return true;
+		}
+	}
 }
