ed72947c69528cee1a0fc09a8a27341ea3d06d32
VTXSRB <jiapetardefx@gmail.com>
2024-03-09 01:23:58 +0100

    asdasdasd

--- CS2StratRoulette/Strategies/Phoon.cs
+++ CS2StratRoulette/Strategies/Phoon.cs
@@ -7,12 +7,12 @@ namespace CS2StratRoulette.Strategies
     [SuppressMessage("ReSharper", "UnusedType.Global")]
     public class Phoon : IStrategy
     {
-        /// <inheritdoc cref="IStrategy.Name"/>
-        public string Name => "Phoon";
-
         private const string enablePh = "sv_cheats 1;sv_enablebunnyhopping 1;sv_maxvelocity 7000;sv_staminamax 0;sv_staminalandcost 0;sv_staminajumpcost 0;sv_accelerate_use_weapon_speed 0;sv_staminarecoveryrate 0;sv_autobunnyhopping 1;sv_airaccelerate 2000;sv_cheats 0";
 
         private const string disablePh = "sv_cheats 1;sv_enablebunnyhopping 0;sv_maxvelocity 3500;sv_staminamax 80;sv_staminalandcost 0.05;sv_staminajumpcost 0.08;sv_accelerate_use_weapon_speed 1;sv_staminarecoveryrate 60;sv_autobunnyhopping 0;sv_airaccelerate 12;sv_cheats 0";
+
+        /// <inheritdoc cref="IStrategy.Name"/>
+        public string Name => "Phoon";
 
         /// <inheritdoc cref="IStrategy.Description"/>
         public string Description =>
