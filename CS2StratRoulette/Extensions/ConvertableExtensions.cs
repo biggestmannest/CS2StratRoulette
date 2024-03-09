e6b20ec0b01c7268c52281b8865788b16ca0e9e9
biggestmannest <54690435+biggestmannest@users.noreply.github.com>
2024-03-09 23:52:21 +0100

    fix

--- CS2StratRoulette/Extensions/ConvertableExtensions.cs
+++ CS2StratRoulette/Extensions/ConvertableExtensions.cs
@@ -5,12 +5,12 @@ namespace CS2StratRoulette.Extensions
 {
 	public static class ConvertableExtensions
 	{
-		public static string Str<T>(this T @this, IFormatProvider? provider) where T : struct, IConvertible
+		public static string Str<T>(this T @this, IFormatProvider? provider = null) where T : struct, IConvertible
 		{
 			return @this.ToString(provider ?? CultureInfo.InvariantCulture);
 		}
 
-		public static string Str<T>(this T? @this, IFormatProvider? provider) where T : struct, IConvertible
+		public static string Str<T>(this T? @this, IFormatProvider? provider = null) where T : struct, IConvertible
 		{
 			return @this?.ToString(provider ?? CultureInfo.InvariantCulture) ?? string.Empty;
 		}
