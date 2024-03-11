b2ab4be3181d8a2cf8c18f726e086513fd0f876e
biggestmannest <54690435+biggestmannest@users.noreply.github.com>
2024-03-11 19:58:59 +0100

    refactor

--- CS2StratRoulette/Strategies/DoT.cs
+++ CS2StratRoulette/Strategies/DoT.cs
@@ -3,40 +3,40 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace CS2StratRoulette.Strategies
 {
-  [SuppressMessage("ReSharper", "UnusedType.Global")]
-  public sealed class DoT : Strategy
-  {
-    private const string Activate = "mp_global_damage_per_second 0";
-    private const string Deactivate = "mp_global_damage_per_second 1";
+	[SuppressMessage("ReSharper", "UnusedType.Global")]
+	public sealed class DoT : Strategy
+	{
+		private const string Activate = "mp_global_damage_per_second 0";
+		private const string Deactivate = "mp_global_damage_per_second 1";
 
-    public override string Name =>
-      "Damage over time";
+		public override string Name =>
+			"Damage over time";
 
-    public override string Description =>
-      "Kill or be killed by The Council.";
+		public override string Description =>
+			"Kill or be killed by The Council.";
 
-    public override bool Start(ref CS2StratRoulettePlugin plugin)
-    {
-      if (!base.Start(ref plugin))
-      {
-        return false;
-      }
+		public override bool Start(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Start(ref plugin))
+			{
+				return false;
+			}
 
-      Server.ExecuteCommand(DoT.Activate);
+			Server.ExecuteCommand(DoT.Activate);
 
-      return true;
-    }
+			return true;
+		}
 
-    public override bool Stop(ref CS2StratRoulettePlugin plugin)
-    {
-      if (!base.Stop(ref plugin))
-      {
-        return false;
-      }
+		public override bool Stop(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Stop(ref plugin))
+			{
+				return false;
+			}
 
-      Server.ExecuteCommand(DoT.Deactivate);
+			Server.ExecuteCommand(DoT.Deactivate);
 
-      return true;
-    }
-  }
+			return true;
+		}
+	}
 }
--- CS2StratRoulette/Strategies/PlantAnywhere.cs
+++ CS2StratRoulette/Strategies/PlantAnywhere.cs
@@ -2,40 +2,40 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace CS2StratRoulette.Strategies
 {
-  [SuppressMessage("ReSharper", "UnusedType.Global")]
-  public sealed class PlantAnywhere : Strategy
-  {
-    private const string Enable = "mp_plant_c4_anywhere 1; mp_c4timer 60";
-    private const string Disabled = "mp_plant_c4_anywhere 0; mp_c4timer 40";
+	[SuppressMessage("ReSharper", "UnusedType.Global")]
+	public sealed class PlantAnywhere : Strategy
+	{
+		private const string Enable = "mp_plant_c4_anywhere 1; mp_c4timer 60";
+		private const string Disable = "mp_plant_c4_anywhere 0; mp_c4timer 40";
 
-    public override string Name =>
-      "Plant Anywhere";
+		public override string Name =>
+			"Plant Anywhere";
 
-    public override string Description =>
-      "The bomb may be planted anywhere.";
+		public override string Description =>
+			"The bomb may be planted anywhere.";
 
-    public override bool Start(ref CS2StratRoulettePlugin plugin)
-    {
-      if (!base.Start(ref plugin))
-      {
-        return false;
-      }
+		public override bool Start(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Start(ref plugin))
+			{
+				return false;
+			}
 
-      CounterStrikeSharp.API.Server.ExecuteCommand(PlantAnywhere.Enable);
+			CounterStrikeSharp.API.Server.ExecuteCommand(PlantAnywhere.Enable);
 
-      return true;
-    }
+			return true;
+		}
 
-    public override bool Stop(ref CS2StratRoulettePlugin plugin)
-    {
-      if (!base.Stop(ref plugin))
-      {
-        return false;
-      }
+		public override bool Stop(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Stop(ref plugin))
+			{
+				return false;
+			}
 
-      CounterStrikeSharp.API.Server.ExecuteCommand(PlantAnywhere.Disabled);
+			CounterStrikeSharp.API.Server.ExecuteCommand(PlantAnywhere.Disable);
 
-      return true;
-    }
-  }
+			return true;
+		}
+	}
 }
