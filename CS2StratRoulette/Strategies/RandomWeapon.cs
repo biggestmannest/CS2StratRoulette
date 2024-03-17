a4da5f2b9724791b99939e4d2f63c9aab330d39a
biggestmannest <54690435+biggestmannest@users.noreply.github.com>
2024-03-17 20:01:37 +0100

    updates

--- CS2StratRoulette/Strategies/RandomWeapon.cs
+++ CS2StratRoulette/Strategies/RandomWeapon.cs
@@ -1,24 +1,14 @@
+using CS2StratRoulette.Constants;
 using CS2StratRoulette.Extensions;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Entities.Constants;
 using CounterStrikeSharp.API;
 using System.Diagnostics.CodeAnalysis;
-using CS2StratRoulette.Constants;
 
 namespace CS2StratRoulette.Strategies
 {
 	[SuppressMessage("ReSharper", "UnusedType.Global")]
 	public sealed class RandomWeapon : Strategy
 	{
-		private const int PistolMin = (int)CsItem.Deagle;
-		private const int PistolMax = (int)CsItem.Revolver;
-
-		private const int MidMin = (int)CsItem.Mac10;
-		private const int MidMax = (int)CsItem.Negev;
-
-		private const int RifleMin = (int)CsItem.AK47;
-		private const int RifleMax = (int)CsItem.G3SG1;
-
 		public override string Name =>
 			"Random weapon";
 
@@ -37,14 +27,9 @@ namespace CS2StratRoulette.Strategies
 			Server.ExecuteCommand(Commands.BuyAllowNone);
 			Server.ExecuteCommand(Commands.BuyAllowGrenadesDisable);
 
-			foreach (var player in Utilities.GetPlayers())
+			foreach (var controller in Utilities.GetPlayers())
 			{
-				if (!player.TryGetPlayerController(out var controller))
-				{
-					continue;
-				}
-
-				if (!controller.TryGetPlayerPawn(out var pawn) || pawn.WeaponServices is null)
+				if (!controller.TryGetPlayerPawn(out var pawn))
 				{
 					continue;
 				}
@@ -55,14 +40,9 @@ namespace CS2StratRoulette.Strategies
 					CSWeaponType.WEAPONTYPE_EQUIPMENT
 				);
 
-				var item = this.random.Next(3) switch
-				{
-					0 => (CsItem)this.random.Next(RandomWeapon.PistolMin, RandomWeapon.PistolMax),
-					1 => (CsItem)this.random.Next(RandomWeapon.MidMin, RandomWeapon.MidMax),
-					_ => (CsItem)this.random.Next(RandomWeapon.RifleMin, RandomWeapon.RifleMax),
-				};
+				var item = Items.Weapons[this.random.Next(Items.Weapons.Length)];
 
-				player.GiveNamedItem(item);
+				controller.GiveNamedItem(item);
 			}
 
 			return true;
