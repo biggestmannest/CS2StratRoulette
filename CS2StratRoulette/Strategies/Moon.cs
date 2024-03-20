c8d0bf9191dbf252df6ef685d7e412e8c2a47240
biggestmannest <54690435+biggestmannest@users.noreply.github.com>
2024-03-20 22:36:57 +0100

    updates

---
+++ CS2StratRoulette/Strategies/Moon.cs
@@ -0,0 +1,45 @@
+using CounterStrikeSharp.API;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CS2StratRoulette.Strategies
+{
+	[SuppressMessage("ReSharper", "UnusedType.Global")]
+	public sealed class Moon : Strategy
+	{
+		private const string Enable =
+			"sv_cheats 1;sv_gravity 230;sv_airaccelerate 20000;sv_maxspeed 420;sv_friction 4;sv_cheats 0";
+
+		private const string Disable =
+			"sv_cheats 1;sv_gravity 800;sv_airaccelerate 12;sv_maxspeed 320;sv_friction 5.2;sv_cheats 0";
+
+		public override string Name =>
+			"Moon";
+
+		public override string Description =>
+			"It's the moon.";
+
+		public override bool Start(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Start(ref plugin))
+			{
+				return false;
+			}
+
+			Server.ExecuteCommand(Moon.Enable);
+
+			return true;
+		}
+
+		public override bool Stop(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Stop(ref plugin))
+			{
+				return false;
+			}
+
+			Server.ExecuteCommand(Moon.Disable);
+
+			return true;
+		}
+	}
+}
