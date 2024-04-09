b5ca06f9986df9a21eaff1d1c005eadcc5470095
VTXSRB <jiapetardefx@gmail.com>
2024-04-09 17:10:59 +0200

    more strategies

---
+++ CS2StratRoulette/Strategies/DamageTeleport.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using CounterStrikeSharp.API.Core;
+using System.Diagnostics.CodeAnalysis;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Modules.Utils;
+using CS2StratRoulette.Constants;
+using CS2StratRoulette.Extensions;
+
+namespace CS2StratRoulette.Strategies
+{
+	[SuppressMessage("ReSharper", "UnusedType.Global")]
+	public sealed class DamageTeleport : Strategy
+	{
+		public override string Name =>
+			"aghhhh!!! dont shoot!!!!!";
+
+		public override string Description =>
+			"You teleport to a random place when you are shot.";
+
+		private static readonly Dictionary<string, Vector[]> Maps = new(System.StringComparer.OrdinalIgnoreCase)
+		{
+			{ "de_mirage", RandomTPs.Mirage },
+			{ "de_overpass", RandomTPs.Overpass },
+			{ "de_nuke", RandomTPs.Nuke },
+			{ "de_dust2", RandomTPs.Dust2 },
+			{ "cs_italy", RandomTPs.Italy },
+			{ "de_vertigo", RandomTPs.Vertigo },
+		};
+		
+		private static readonly System.Random Random = new();
+		
+		public override bool Start(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Start(ref plugin))
+			{
+				return false;
+			}
+
+            plugin.RegisterEventHandler<EventPlayerHurt>(this.OnHurt);
+            
+			return true;
+		}
+
+		public override bool Stop(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Stop(ref plugin))
+			{
+				return false;
+			}
+
+			const string playerHurt = "player_hurt";
+			
+			plugin.DeregisterEventHandler(playerHurt, this.OnHurt, false);
+			
+			return true;
+		}
+
+		private HookResult OnHurt(EventPlayerHurt @event, GameEventInfo _)
+		{
+			var serverMap = Server.MapName;
+
+			if (!DamageTeleport.Maps.TryGetValue(serverMap, out var spots))
+			{
+				return HookResult.Continue;
+			}
+			
+			DamageTeleport.Random.Shuffle(spots);
+
+			var player = @event.Userid;
+            
+			var n = spots.Length;
+			
+				if (!player.TryGetPlayerPawn(out var pawn))
+				{
+					return HookResult.Continue;
+				}
+
+				if (n <= 0)
+				{
+					n = spots.Length;
+				}
+
+				var position = spots[--n];
+				var angle = pawn.V_angle;
+				
+				Server.NextFrame(() =>
+				{
+					pawn.Teleport(
+						position,
+						angle,
+						VectorExtensions.Zero
+					);
+				});
+
+			return HookResult.Continue;
+		}
+	}
+}
---
+++ CS2StratRoulette/Strategies/SmokeStrat.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Modules.Utils;
+using CS2StratRoulette.Constants;
+
+namespace CS2StratRoulette.Strategies
+{
+	[SuppressMessage("ReSharper", "UnusedType.Global")]
+	public sealed class SmokeStrat : Strategy
+	{
+		public override string Name =>
+			"Smoke Strat";
+
+		public override string Description =>
+			"was it a jump throw or a run throw?? or a normal throw... whats the lineup again???";
+
+		private static readonly Dictionary<string, Vector[]> Maps = new(System.StringComparer.OrdinalIgnoreCase)
+		{
+			{ "de_dust2", SmokeSpots.Dust2 },
+			{ "de_mirage", SmokeSpots.Mirage },
+			{ "de_nuke", SmokeSpots.Nuke },
+			{ "de_vertigo", SmokeSpots.Vertigo },
+			{ "cs_italy", SmokeSpots.Italy }
+		};
+		
+		public override bool Start(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Start(ref plugin))
+			{
+				return false;
+			}
+
+			var serverMap = Server.MapName;
+
+			if (!SmokeStrat.Maps.TryGetValue(serverMap, out var spots))
+			{
+				return false;
+			}
+
+			Vector velocity = new(0f, 0f, 0f);
+			QAngle angle = new(0f, 0f, 0f);
+            
+			foreach (var position in spots)
+			{
+				Signatures.CreateSmoke.Invoke(
+					position.Handle,
+					angle.Handle,
+					velocity.Handle,
+					velocity.Handle,
+					IntPtr.Zero,
+					45,
+					2
+				);
+			}
+			
+			return true;
+		}
+
+		public override bool Stop(ref CS2StratRoulettePlugin plugin)
+		{
+			if (!base.Stop(ref plugin))
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
