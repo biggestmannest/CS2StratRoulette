6a5a38141b2cc0acb826e9b77d3c5a9bd2f213bd
biggestmannest <54690435+biggestmannest@users.noreply.github.com>
2024-04-10 19:25:29 +0200

    net8

--- CS2StratRoulette/Strategies/Knockback.cs
+++ CS2StratRoulette/Strategies/Knockback.cs
@@ -1,3 +1,5 @@
+using System.Collections.Frozen;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Utils;
@@ -8,9 +10,50 @@ namespace CS2StratRoulette.Strategies
 	[SuppressMessage("ReSharper", "UnusedType.Global")]
 	public sealed class Knockback : Strategy
 	{
-		private const float MaxWeight = 30f;
-
 		private static readonly Vector Velocity = new(200f, 200f, 200f);
+
+		private static readonly FrozenDictionary<string, float> Weights =
+			new Dictionary<string, float>(System.StringComparer.OrdinalIgnoreCase)
+			{
+				{ "weapon_deagle", .5f },
+				{ "weapon_revolver", .4f },
+				{ "weapon_glock", .2f },
+				{ "weapon_usp_silencer", .1f },
+				{ "weapon_cz75a", .2f },
+				{ "weapon_fiveseven", .25f },
+				{ "weapon_p250", .2f },
+				{ "weapon_tec9", .25f },
+				{ "weapon_elite", .2f },
+				{ "weapon_hkp2000", .2f },
+
+				{ "weapon_mp9", .4f },
+				{ "weapon_mac10", .4f },
+				{ "weapon_bizon", .35f },
+				{ "weapon_mp7", .4f },
+				{ "weapon_ump45", .4f },
+				{ "weapon_p90", .4f },
+				{ "weapon_mp5sd", .3f },
+
+				{ "weapon_famas", .5f },
+				{ "weapon_galilar", .5f },
+				{ "weapon_m4a4", .6f },
+				{ "weapon_m4a1_silencer", .45f },
+				{ "weapon_ak47", .6f },
+				{ "weapon_aug", .65f },
+				{ "weapon_sg553", .65f },
+				{ "weapon_ssg08", .55f },
+				{ "weapon_awp", .8f },
+				{ "weapon_scar20", .7f },
+				{ "weapon_g3sg1", .7f },
+
+				{ "weapon_nova", .65f },
+				{ "weapon_xm1014", .65f },
+				{ "weapon_mag7", .65f },
+				{ "weapon_sawedoff", .65f },
+				{ "weapon_m249", .65f },
+
+				{ "weapon_negev", .5f },
+			}.ToFrozenDictionary();
 
 		public override string Name =>
 			"Knockback II";
@@ -57,39 +100,23 @@ namespace CS2StratRoulette.Strategies
 				return HookResult.Continue;
 			}
 
-			var designerName = @event.Weapon;
-			CBasePlayerWeaponVData? wData = null;
+			var weapon = @event.Weapon;
 
-			System.Console.WriteLine($"[Knockback::OnWeaponFire] event_weapon {designerName}");
+			System.Console.WriteLine($"[Knockback::OnWeaponFire] event_weapon {weapon}");
 
-			pawn.ForEachWeapon((w) =>
+			if (weapon is null || !Knockback.Weights.TryGetValue(weapon, out var weight))
 			{
-				System.Console.WriteLine($"[Knockback::OnWeaponFire] pawn_weapon {w.DesignerName}");
-				if (string.Equals(w.DesignerName, designerName, System.StringComparison.OrdinalIgnoreCase) &&
-					w.TryGetData(out var data))
-				{
-					System.Console.WriteLine($"[Knockback::OnWeaponFire] found {w.DesignerName}");
-					wData = data;
-				}
-			});
-
-			if (wData is null)
-			{
-				System.Console.WriteLine($"[Knockback::OnWeaponFire] wData is null");
-				return HookResult.Continue;
+				weight = 0f;
 			}
 
 			var vAngle = pawn.V_angle;
 			var velocity = pawn.AbsVelocity;
-			var knockback = (wData.Weight / Knockback.MaxWeight);
 
 			var forward = new Vector(0f, 0f, 0f);
 
 			NativeAPI.AngleVectors(vAngle.Handle, forward.Handle, System.IntPtr.Zero, System.IntPtr.Zero);
 
-			velocity += (Knockback.Velocity * knockback).Multi(forward * -1f);
-
-			System.Console.WriteLine($"[Knockback::OnWeaponFire] {velocity} / {knockback.Str()}");
+			velocity += (Knockback.Velocity * weight).Multi(forward * -1f);
 
 			pawn.Teleport(
 				pawn.AbsOrigin ?? VectorExtensions.Zero,
